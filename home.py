3b35504b63a17a0b8f78b4e1a18ced5ac4dd1e39
Jumani Clarke <jumaniclarke@gmail.com>
2025-12-17 11:35:37 +0200

    chnages with image placement

--- home.py
+++ home.py
@@ -8,8 +8,8 @@ if not st.user.is_logged_in:
 else:
     if st.button("Logout"):
         st.logout()
+    st.image(st.user.picture)
 
 st.write(st.user)
-st.image(st.user.picture)
 st.write("You are logged in as " + st.user.name if st.user.is_logged_in else "You are not logged in.")
 
