39be656a9e1dba006420f143c53d1ebfcb98a351
Jumani Clarke <jumaniclarke@gmail.com>
2025-12-17 11:26:02 +0200

    another update

--- home.py
+++ home.py
@@ -5,6 +5,9 @@ st.title("Authentication")
 if not st.user.is_logged_in:
     if st.button("Authentication"):
         st.login("google")
+else:
+    if st.button("Logout"):
+        st.logout()
 
 st.write(st.user)
 st.write("You are logged in as " + st.user.name if st.user.is_logged_in else "You are not logged in.")
