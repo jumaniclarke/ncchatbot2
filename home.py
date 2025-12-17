807884ea747fba94c75269f671d24c2b9d7102c6
Jumani Clarke <jumaniclarke@gmail.com>
2025-12-17 07:10:06 +0200

    First commit

---
+++ home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Authentication")
+
+if not st.user.is_logged_in:
+    if st.button("Authentication"):
+        st.login("google")
+
+st.write(st.user)
+st.write("You are logged in as " + st.user.name if st.user.is_logged_in else "You are not logged in.")
+
